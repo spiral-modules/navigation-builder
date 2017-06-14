9f99925ba9395600d9db143886a207ad4582068c
Valentin <vintsukevich@gmail.com>
2017-06-14 15:27:00 +0300

    M structure navigation

--- source/NavigationBuilder/Builders/HtmlBuilder.php
+++ source/NavigationBuilder/Builders/HtmlBuilder.php
@@ -2,13 +2,11 @@
 
 namespace Spiral\NavigationBuilder\Builders;
 
-use Spiral\NavigationBuilder\DefaultRenderer;
 use Spiral\NavigationBuilder\RendererInterface;
-use Spiral\NavigationBuilder\Services\LinkWrapper;
 
-class HtmlBuilder extends TreeBuilder
+class HtmlBuilder extends StructureBuilder
 {
-    /** @var DefaultRenderer */
+    /** @var RendererInterface */
     private $renderer;
 
     /**
@@ -28,42 +26,5 @@ class HtmlBuilder extends TreeBuilder
         $navigation = parent::build($domain);
 
         return $this->renderer->navigation($navigation);
-    }
-
-    /**
-     * @param array $data
-     * @return array
-     */
-    protected function packRow(array $data): array
-    {
-        return parent::packRow($data) + [
-            'depth'      => $data['depth'],
-            'text'       => $data['link']['text'],
-            'href'       => $data['link']['href'],
-            'attributes' => LinkWrapper::unpackAttributes($data['link']['attributes']),
-        ];
-    }
-
-    /**
-     * @param array $data
-     * @return array
-     */
-    protected function packLink(array $data): array
-    {
-        return parent::packLink($data) + [
-            'depth'      => $data['depth'],
-            'text'       => $data['text'],
-            'href'       => $data['href'],
-            'attributes' => $data['attributes'],
-        ];
-    }
-
-    /**
-     * @param string $domain
-     * @return array
-     */
-    protected function getTree(string $domain): array
-    {
-        return $this->source->findDomainTree($domain)->orderBy('order', 'ASC')->fetchData();
     }
 }
