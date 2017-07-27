7c74b57da75fc9fb550e3fa6dc4668334a0f2375
Valentin V <vvval@users.noreply.github.com>
2017-07-27 17:22:11 +0300

    Update list.dark.php

--- source/views/vault/list.dark.php
+++ source/views/vault/list.dark.php
@@ -1,7 +1,44 @@
-<?php
+<extends:vault.layout title="[[Navigation]]" class="wide-content"/>
+
+<?php #compile
 /**
- * Created by PhpStorm.
- * User: Valentin
- * Date: 25.05.2017
- * Time: 11:28
+ * @var array $domains
  */
+?>
+
+<define:actions>
+    <select name="domain" class="item-select js-select-domain">
+        <?php foreach ($domains as $name => $domain) { ?>
+            <option value="<?= $name ?>"><?= $domain ?></option>
+        <?php } ?>
+    </select>
+</define:actions>
+
+<define:content>
+    <div class="card">
+        <div class="card-content">
+            <div class="row">
+                <div class="col s12 m7">
+                    <p>Drag and drop to reorder menu. Use checkboxes to enable disable section</p>
+                </div>
+
+                <div class="col s12 m4 offset-m1">
+                    <p>Links are independent to navigation, if you change link title or URL it is changed everywhere it is used. Use different links if you need different titles for same URL.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col s12 m7">
+                    <div class="editable-navigation js-editable-navigation js-sf-navigation-items" data-domain="default">
+                        Loading navigation...
+                    </div>
+                </div>
+
+                <div class="col s12 m4 offset-m1">
+                    <div class="js-link-list js-sf-navigation-items">
+                        Loading links...
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</define:content>
